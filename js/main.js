649481b64c04080cc37fa5fc2cbaf69b01d373b0
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2021-10-22 10:51:06 +0200

    Added Services and Our-skills Sections

--- js/main.js
+++ js/main.js
@@ -14,13 +14,19 @@ menuBtn.addEventListener("click", function () {
 let megaMenu = document.querySelector(".mega-menu");
 let links = document.querySelector("header .menu > li:last-Child");
 let a = document.querySelector("header .menu > li:last-Child a");
+let articles = document.querySelector("#articles");
+let heading = document.querySelector("#articles .main-heading");
 
 links.addEventListener("click", function (e) {
   if (e.target === a) {
     if (megaMenu.classList.contains("show")) {
       megaMenu.classList.remove("show");
+      articles.style.zIndex = "100";
+      heading.style.zIndex = "100";
     } else {
       megaMenu.classList.add("show");
+      articles.style.zIndex = "0";
+      heading.style.zIndex = "0";
     }
   }
 });
