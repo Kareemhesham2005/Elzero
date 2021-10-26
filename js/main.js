8b2221a4ddb1b6d79c544ee913aba453f22607dd
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2021-10-27 01:58:38 +0200

    Added countdown

--- js/main.js
+++ js/main.js
@@ -50,3 +50,22 @@ inputs.forEach((input) => {
     }
   };
 });
+
+let countDownDate = new Date("Dec 31, 2021 23:59:59").getTime();
+
+let counter = setInterval( () => {
+  let datenow = new Date().getTime();
+  let date = countDownDate - datenow 
+  
+  let days = Math.floor(date / 1000 / 60 / 60 / 24);
+  let hours = Math.floor(date % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+ let minutes = Math.floor(date % (1000 * 60 * 60) / (1000 * 60));
+ let seconds = Math.floor(date % (1000 * 60) / (1000));
+ document.querySelector(".days").innerHTML = days < 10 ? `0${days}`:days;
+ document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}`:hours;
+ document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}`:minutes;
+ document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}`:seconds;
+ if (date < 0) {
+   clearInterval(counter);
+ }
+},1000);
